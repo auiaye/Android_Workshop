85cb8024d617a1c900a87ebde18606e66473e10b
Auiaye@Home <bunyong@hotmail.com>
2018-09-05 21:33:56 +0700

    Update Design and Script

--- app/src/main/java/com/arit/demo/demo01/MainActivity.java
+++ app/src/main/java/com/arit/demo/demo01/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.math.BigDecimal;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,25 +18,55 @@ public class MainActivity extends AppCompatActivity {
 //    public TextView tvMessage;
     @BindView(R.id.tvMessage) TextView tvMessage;
     @BindView(R.id.txtHeight) EditText txtHeight;
-    @BindView(R.id.tvWeight) EditText tvWeight;
+    @BindView(R.id.txtWeight) EditText txtWeight;
 //    @BindView(R.id.btnHello) Button btnHello;
+    private static DecimalFormat formatter = new DecimalFormat();
 
-    @OnClick(R.id.btnHello)
-    public void doClickHello() {
-        this.tvMessage.setText(R.string.helloMessage);
-    }
 
-    @OnClick(R.id.btnHello2)
-    public void doClickHello2(){
-//        this.tvMessage.setText(R.string.helloMessage2);
-        double strHeight;
-        double strWeight;
-        strHeight = Double.parseDouble(this.txtHeight.getText().toString());
-        strWeight = Double.parseDouble(this.tvWeight.getText().toString());
-        this.tvMessage.setText(Double.toString(strHeight));
+    @OnClick(R.id.btnCancel)
+    public void doClickClear() {
+        this.txtHeight.setText("");
+        this.txtWeight.setText("");
+        this.tvMessage.setText("---Result---");
     }
 
 
+    @OnClick(R.id.btnCalculate)
+    public void doClickCalculated(){
+        double dblHeight, dblWeight, numBMI;
+        String getCalculate;
+        int decimalPlaces = 2;
+        dblHeight = Double.parseDouble(this.txtHeight.getText().toString());
+        dblWeight = Double.parseDouble(this.txtWeight.getText().toString());
+        numBMI = (dblWeight/((dblHeight/100)*2));
+        BigDecimal bd = new BigDecimal(numBMI);
+        bd = bd.setScale(decimalPlaces, BigDecimal.ROUND_HALF_UP);
+        numBMI = bd.doubleValue();
+        getCalculate = Calculate_BMI(numBMI);
+        this.tvMessage.setText("BMI : " + getCalculate);
+    }
+
+    public static String Calculate_BMI(double numBMI) {
+        String ResultCal;
+        if (numBMI < 16) {
+            ResultCal = "Skin and bones";
+        }else if (numBMI >= 16 && numBMI < 17) {
+            ResultCal = "Thin";
+        }else if (numBMI >= 17 && numBMI < 18.5) {
+            ResultCal = "Fit";
+        }else if (numBMI >= 18.5 && numBMI < 25) {
+            ResultCal = "Good shape";
+        }else if (numBMI >= 25 && numBMI < 30) {
+            ResultCal = "Over Weight";
+        }else if (numBMI >= 30 && numBMI < 35) {
+            ResultCal = "Chubby";
+        }else if (numBMI >= 35 && numBMI < 40) {
+            ResultCal = "Fluffy";
+        } else {
+            ResultCal = "Tubby";
+        }
+        return ResultCal;
+    }
 
 //    public static double convertToDouble(String temp){
 //        String a = temp;
@@ -52,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
 
 //        2018-09-02
 //        tvMessage =  findViewById(R.id.tvMessage);
